1b4ee3cbaac16b36c1681e8dd944118008b1aa86
Rene Quinteros <reny2012@outlook.com>
2023-03-25 00:53:21 -0300

    primer commit clase3

---
+++ app.js
@@ -0,0 +1,26 @@
+function dato(){
+    return "rene";
+}
+const nonbre= dato();
+dato()
+console.log(nonbre)
+
+if(5>3){
+    console.log( "la condicion es verdadera")
+} else{ console.log("la condicion es falsa")}
+
+if(5>6){ console.log("A")}
+
+    else if(5>7){ console.log("B")}
+    else if(5>6){console.log("C")}
+    else { console.log("D")}
+
+    let dia= "viernes"
+
+    switch (dia){
+        case "lunes":
+            console.log("odio mi vida")
+            break;
+        case "viernes": 
+            console.log("hoy es dis de joda")
+    }
